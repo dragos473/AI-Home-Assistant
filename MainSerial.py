5ad95c105548950e83f50e89eaa40847a74900f0
Ciobanu Dragos Alexandru <dragosciobanu00@gmail.com>
2026-01-25 21:28:06 +0200

    added AI responses and linked the arduino to the Gemini API

--- MainSerial.py
+++ MainSerial.py
@@ -1,17 +1,25 @@
 import speech_recognition as sr
+import serial
+import time
+from google import genai
+GEMINI_API_KEY = "AIzaSyAbNjVk6DtIv5Yjf6tNtfikxus9Cthdbfo"
 
 rec = sr.Recognizer()
+arduino  = serial.Serial('COM3', 9600, timeout=1)
+time.sleep(2)
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 with sr.Microphone(device_index= 2) as source:
     rec.adjust_for_ambient_noise(source, duration=2)
     rec.dynamic_energy_threshold = True
-    rec.pause_threshold = 1.5
+    rec.pause_threshold = 0.8
     print("Setup done!")
     while (1):
         try: 
             audio = rec.listen(source)
-            msg =  rec.recognize_google(audio, language="ro-RO")
-            print(msg)
+            prompt =  rec.recognize_google(audio, language="en-US")
+            msg = client.models.generate_content(model = "gemini-2.5-flash-lite", contents = "Respond in exactly 30 characters." + prompt).text
+            arduino.write(bytes(msg, 'utf-8'))
         except sr.UnknownValueError:
             print("Sorry, I could not understand the audio.")
         except sr.RequestError as e:
